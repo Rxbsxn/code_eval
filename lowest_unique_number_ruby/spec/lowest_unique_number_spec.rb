7917a97d17a006e58e880419c5eca3394c471210
Robert Harężlak <harezlak.robert7@gmail.com>
2017-08-17 22:43:02 +0200

    first step

--- lowest_unique_number_ruby/spec/lowest_unique_number_spec.rb
+++ lowest_unique_number_ruby/spec/lowest_unique_number_spec.rb
@@ -1,4 +1,20 @@
 require_relative ".././lib/lowest_unique_number"
 RSpec.describe LowestUniqueNumber do
+  describe '.result' do
+    context 'five players in game, one winner' do
+      it 'returns winner' do
+        collection = [1, 2, 3, 4, 5]
 
+        expect(LowestUniqueNumber.result(collection).to eq 1)
+      end
+    end
+
+    context 'six players in game, no winner' do
+      it 'returns winner' do
+        collection = [1, 2, 2, 3, 1, 3]
+
+        expect(LowestUniqueNumber.result(collection).to eq 0)
+      end
+    end
+  end
 end
