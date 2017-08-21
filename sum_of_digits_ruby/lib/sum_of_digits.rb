c862fcd79c98c0c1f9ccdd008ce0646cb9fba113
Robert Harężlak <harezlak.robert7@gmail.com>
2017-08-22 00:53:41 +0200

    solution for sum_of_digits

--- sum_of_digits_ruby/lib/sum_of_digits.rb
+++ sum_of_digits_ruby/lib/sum_of_digits.rb
@@ -1,2 +1,16 @@
 class SumOfDigits
+  attr_reader :string
+  def initialize(string)
+    @string = string
+  end
+
+  def sum
+    string.chars.map(&:to_i).reduce(:+)
+  end
+end
+
+unless ENV['RACK_ENV'] == 'rspec'
+  File.open(ARGV[0]).each_line do |line|
+    puts SumOfDigits.new(line).sum
+  end
 end
--- sum_of_digits_ruby/spec/sum_of_digits_spec.rb
+++ sum_of_digits_ruby/spec/sum_of_digits_spec.rb
@@ -1,3 +1,8 @@
 require_relative ".././lib/sum_of_digits"
 RSpec.describe SumOfDigits do
+  describe '#sum' do
+    it "sums given number's digits" do
+      expect(SumOfDigits.new('12').sum).to eq 3
+    end
+  end
 end
